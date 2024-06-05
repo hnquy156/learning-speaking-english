23450c52e2f561ec8a1e42ee3e72ca2170a97eb6
hnquy156 <hnquy.hbt@gmail.com>
2024-06-05 22:46:50 +0700

    Add ditacphone

--- frontend/app/chat/page.js
+++ frontend/app/chat/page.js
@@ -1,15 +1,28 @@
 'use client';
 import { useEffect, useState } from 'react';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
 import { createChat, deleteChat, getChat, getChats, updateChat } from './api';
 import Spinner from '../components/Spinner';
 import DeleteIcon from '../components/icons/DeleteIcon';
 import { CHAT_ROLES } from '../utils/constant';
+import MicrophoneIcon from '../components/icons/MicrophoneIcon';
+import StopIcon from '../components/icons/StopIcon';
 
 export default function Chat() {
   const [chats, setChats] = useState([]);
   const [selectedChat, setSelectedChat] = useState(null);
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    finalTranscript,
+    interimTranscript,
+  } = useSpeechRecognition();
 
   useEffect(() => {
     fetchAllChats();
@@ -32,6 +45,13 @@ export default function Chat() {
     }, 300);
     return () => clearTimeout(timeout);
   }, [selectedChat]);
+
+  useEffect(() => {
+    if (browserSupportsSpeechRecognition && !listening) {
+      const text = document.getElementById('text');
+      text.value = `${text.value} ${finalTranscript}`.trim();
+    }
+  }, [listening]);
 
   const fetchAllChats = async () => {
     try {
@@ -89,8 +109,17 @@ export default function Chat() {
     }
   };
 
+  const handleRecord = async () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening();
+      // { continuous: true }
+    }
+  };
+
   return (
-    <div className="grid grid-cols-12 h-full">
+    <div className="grid grid-cols-12 h-full" suppressHydrationWarning={true}>
       <div className="col-span-3 bg-slate-50 h-full flex flex-col">
         <button
           className="self-center border-gray-300 border-2 rounded-lg p-1 m-2 w-5/6 hover:bg-slate-100"
@@ -98,15 +127,15 @@ export default function Chat() {
         >
           + Add new chat
         </button>
-        <ul>
+        <div>
           {chats.map((chat) => (
             <div key={chat._id} className="group m-2 p-1 relative">
-              <li
+              <div
                 className="hover:bg-slate-100 cursor-pointer line-clamp-4"
                 onClick={() => handleViewChat(chat._id)}
               >
                 {chat.messages[1].content}
-              </li>
+              </div>
               <div className="hidden group-hover:flex justify-end mr-4 absolute right-1 bottom-1/2 translate-y-1/2">
                 <button
                   className="p-2 bg-slate-200 cursor-pointer opacity-70 rounded-full"
@@ -117,7 +146,7 @@ export default function Chat() {
               </div>
             </div>
           ))}
-        </ul>
+        </div>
       </div>
       <div className="col-span-9 bg-white flex flex-col justify-between p-4">
         <div className="font-bold text-2xl">Chat Content</div>
@@ -137,18 +166,28 @@ export default function Chat() {
             </div>
           ))}
         </div>
-        {loading && (
-          <div className="self-center m-2">
-            <Spinner />
-          </div>
+        {browserSupportsSpeechRecognition && (
+          <button
+            className="self-center hover:bg-slate-100 opacity-70 rounded-full p-2"
+            onClick={handleRecord}
+          >
+            {listening ? <StopIcon /> : <MicrophoneIcon />}
+          </button>
         )}
-        <textarea
-          id="text"
-          className="border-solid border-2 focus:outline-none focus:border-sky-500 rounded-xl min-h-20 max-h-60 w-4/6 self-center p-4"
-          rows={3}
-          onKeyDown={handleKeyDown}
-          disabled={loading}
-        />
+        <div className="min-h-20 max-h-60 w-4/6 self-center relative">
+          <textarea
+            id="text"
+            className="border-solid border-2 focus:outline-none focus:border-sky-500 rounded-xl w-full h-full p-4"
+            rows={3}
+            onKeyDown={handleKeyDown}
+            disabled={loading}
+          />
+          {loading && (
+            <div className="self-center absolute right-1/2 bottom-1/2 translate-y-1/2 translate-x-1/2">
+              <Spinner />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
